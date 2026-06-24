6c49214fd3bacdc2f92926f9de11720bd54fc9ae
rposts <rishi.investigate@gmail.com>
2026-06-24 14:20:33 +0000

    Add mariadb-connector-odbc processing
    Signed-off-by: rposts <rishi.investigate@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# MariaDB Connector/ODBC 3.2
+
+This is a GA release of the MariaDB Connector/ODBC.
+MariaDB Connector/ODBC is released under version 2.1 of the
+GNU Lesser Public License.
+
+Additional information available [here](https://github.com/mariadb-corporation/mariadb-connector-odbc).
+
+>**Note:** Artifacts available in this repository are targeted for `s390x` architecture.
---
+++ src/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e -o pipefail
+read -ra arr <<< "$@"
+version=${arr[1]}
+trap 0 1 2 ERR
+# Extract DISTRO details for tagging
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO="$ID-$VERSION_ID"
+    if [ "$VERSION_CODENAME" != "" ]; then
+        DISTRO="$ID-$VERSION_CODENAME"
+    fi
+fi
+current_dir="$PWD"
+echo $DISTRO > .distro_zab.txt
+sudo apt update; sudo apt install git -y
+# Clone linux-on-ibm-z to keep it current
+git clone https://github.com/linux-on-ibm-z/scripts.git /tmp/linux-on-ibm-z
+bash /tmp/linux-on-ibm-z-scripts/MariaDB-Connector-ODBC/${version}/build_mariadb_connector_odbc.sh -y
+cd mariadb-connector-odbc && make package
+mv mariadb-connector-odbc-${version}-linux-s390x.tar.gz ${current_dir}
+exit 0
